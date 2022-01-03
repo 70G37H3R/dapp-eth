394e149151db7c7dc0978a8dede62e1a12c8f4dd
70G37H3R <nchihminh@gmail.com>
2022-01-03 23:56:08 +0700

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,9 @@
 //https://dev.to/heydamali/a-guide-to-building-testing-and-deploying-your-first-dapp-with-truffle-ethersjs-ganache-and-react-1mh0
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const privateKey = fs.readFileSync(".secret").toString().trim();
+const infuraURL = "<HTTPS ENDPOINT HERE>";
+
 module.exports = {
   contracts_build_directory: './client/src/contracts',
   networks: {
@@ -7,11 +12,18 @@ module.exports = {
      port: 7545,
      network_id: "*",
     },
+    rinkeby: {
+      provider: () => new HDWalletProvider(privateKey, infuraURL),
+      network_id: 3,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   },
-
   compilers: {
     solc: {
-      version: "0.8.9"
+      version: "0.7.1",
     }
-  }
+  },
 };
